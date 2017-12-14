911794a4cfb8b4d976ef3ca6f95c64b025d70e20
Jason Ebbin <jebbin@scottlogic.co.uk>
2017-12-14 15:43:03 +0000

    Session one: Implemented basic firing on a square

--- src/ConsoleBattleship/DomainTransitions.fs
+++ src/ConsoleBattleship/DomainTransitions.fs
@@ -1,14 +1,35 @@
 ﻿module DomainTransitions 
 open System
 open Domain
+open System.Text.RegularExpressions
 
-let updateGameState (state: GameState) = 
-    let grid = state.Grid
-    let random = new Random()
-    let newGrid = 
-        grid
-        |> Array.map (fun row -> 
-            row 
-            |> Array.map (fun _ -> if random.NextDouble() > 0.5 then "X" else "M")
-            ) 
-    {state with Grid = newGrid }
+//https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/active-patterns 
+
+let letters = [|'A'..'J'|]
+
+let updateGameState (input: string) (state: GameState) =
+    let xy = input.ToUpperInvariant().ToCharArray()
+    let x = letters |> Array.tryFindIndex(fun x -> x = xy.[0]) 
+    let y = Int32.Parse (xy.[1].ToString())
+    match x,y with
+    | None,_ -> state
+    | _, y when y > 9 -> state
+    | Some x,y ->
+        let newGrid = state.Grid 
+        newGrid.[x].[y] <- "X"
+        let newState = {state with Grid = newGrid}
+        newState
+
+let updateGameState' (input: string) (state: GameState) =
+    let xy = input.ToUpperInvariant().ToCharArray()
+    let x = letters |> Array.tryFindIndex(fun x -> x = xy.[0]) 
+    let parseSuccess, y = Int32.TryParse (xy.[1].ToString())
+    match x,parseSuccess with
+    | None,_ -> state
+    | _, false -> state
+    | Some x, true when y > 0 && y < 9->
+        let newGrid = state.Grid 
+        newGrid.[x].[y] <- "X"
+        let newState = {state with Grid = newGrid}
+        newState
+    | _ -> state
---
+++ src/ConsoleBattleship/Render.fs
@@ -0,0 +1,17 @@
+﻿module Render
+
+open Domain
+open System
+
+let letters = [|'A'..'J'|] |> Array.map string
+
+let renderGame (state: GameState) = 
+    Console.SetCursorPosition(0, 1)
+    Console.WriteLine("  | 0 1 2 3 4 5 6 7 8 9")
+    Console.WriteLine("--+--------------------")
+    for y = 0 to 9 do 
+        Console.Write(letters.[y] + " | ") 
+        for x = 0 to 9 do 
+            Console.Write(state.Grid.[y].[x] + " ")
+        Console.Write('\n')
+    Console.WriteLine("--+--------------------")
